157ed3201402260d3ba75deef044042ca15ccea6
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-06-16 15:39:29 +0500

    admin first deploy

---
+++ precache-manifest.17c462c7558dff7ca4fca480df14f862.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d651b48a487ddb95649f02f1b2b8797",
+    "url": "/Json/districts.json"
+  },
+  {
+    "revision": "51475e1f3c1e269831fce4cf7e663670",
+    "url": "/Json/kvartals.json"
+  },
+  {
+    "revision": "0e9c7393050d0ffabfb88d9523040574",
+    "url": "/Json/regions.json"
+  },
+  {
+    "revision": "94a0a19750e91d6892ec",
+    "url": "/css/about.df49a289.css"
+  },
+  {
+    "revision": "b0cd00b96837ad989d9f",
+    "url": "/css/app.a7a50844.css"
+  },
+  {
+    "revision": "3f0a849251991f79731e",
+    "url": "/css/chunk-vendors.86dd1494.css"
+  },
+  {
+    "revision": "c07eade600e6297c0008a19d5a8ac81f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a0a19750e91d6892ec",
+    "url": "/js/about.904d8894.js"
+  },
+  {
+    "revision": "b0cd00b96837ad989d9f",
+    "url": "/js/app.1799bd53.js"
+  },
+  {
+    "revision": "3f0a849251991f79731e",
+    "url": "/js/chunk-vendors.319af9a6.js"
+  },
+  {
+    "revision": "5cc7ae5e52993f324f4b5e6c5eca1b3b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
